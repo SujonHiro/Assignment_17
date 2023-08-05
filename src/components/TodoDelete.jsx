f93e0537b6dfe11a35fbca3e2836471246b58f5b
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-08-05 23:08:33 +0600

    todoSlice todolist changes

--- src/components/TodoDelete.jsx
+++ src/components/TodoDelete.jsx
@@ -1,18 +1,19 @@
-import Swal from "sweetalert2"
-import Store from "../../redux/store/Store"
-import { RemoveTodo } from "../../redux/slice/TodoSlices"
-export function TodoDelete(i){
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-            Store.dispatch(RemoveTodo(i))
-        }
-      })
+import Swal from "sweetalert2";
+import store from "../../redux/store/store";
+import { RemoveTodo } from "../../redux/slice/TodoSlices";
+
+export function TodoDelete(id) {
+	Swal.fire({
+		title: "Are you sure?",
+		text: "You won't be able to revert this!",
+		icon: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "Yes, delete it!",
+	}).then((result) => {
+		if (result.isConfirmed) {
+			store.dispatch(RemoveTodo(id));
+		}
+	});
 }
