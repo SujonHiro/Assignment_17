971a2183463f6d629ed81ced0ad24bee20d42a3c
SujonHiro <sujonhowlader67@gmail.com>
2023-08-05 23:38:51 +0600

    complete task

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.css"
 import { Provider } from 'react-redux'
-import store  from '../redux/store/Store.js'
+import store from '../redux/store/Store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
